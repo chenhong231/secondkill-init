fd5e97064a45018504ff12c112e73db5cb366de3
chenhong231 <chenhong231@outlook.com>
2022-07-18 14:08:44 +0800

    add 3-6 get otp code

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -8,17 +8,40 @@ import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone")String telphone) {
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        System.out.println("telphone = , " + telphone + " &otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody
